28fd39fc75a35d1b134c26d7b4317a571f8e0e0a
Roman Artemev <roman_artemev@gaijin.team>
2023-09-28 16:08:06 +0300

    Drop legacy mode from lexer
    This mode was introduced to keep support of interpolated
    strings in legacy compiler. Since the old compiler is removed from quirrel
    legacy mode no longer needed.
    
    Also drop macro system from lexer because decided it has no
    further usage idea by now

--- squirrel/sqlexer.h
+++ squirrel/sqlexer.h
@@ -6,50 +6,11 @@
 
 typedef unsigned char LexChar;
 
-struct SQLexerMacroState
-{
-    bool insideStringInterpolation;
-    SQLEXREADFUNC prevReadF;
-    SQUserPointer prevUserPointer;
-    LexChar prevCurrdata;
-    sqvector<SQChar> macroStr;
-    sqvector<SQChar> macroParams;
-    int macroStrPos;
-
-    SQLexerMacroState(SQAllocContext ctx)
-        : macroStr(ctx), macroParams(ctx)
-    {
-        reset();
-    }
-
-    void reset()
-    {
-        insideStringInterpolation = false;
-        prevReadF = NULL;
-        prevUserPointer = NULL;
-        macroStrPos = 0;
-        prevCurrdata = 0;
-        macroStr.resize(0);
-        macroParams.resize(0);
-    }
-
-    static SQInteger macroReadF(SQUserPointer self)
-    {
-        SQLexerMacroState * s = (SQLexerMacroState *) self;
-        return s->macroStr[s->macroStrPos++];
-    }
-};
-
 using namespace SQCompilation;
 
 enum SQLexerState {
   LS_REGULAR,
   LS_TEMPALTE
-};
-
-enum SQLexerMode {
-  LM_LEGACY,
-  LM_AST
 };
 
 enum SQTokenFlags {
@@ -59,7 +20,7 @@ enum SQTokenFlags {
 
 struct SQLexer
 {
-    SQLexer(SQSharedState *ss, SQCompilationContext &ctx, enum SQLexerMode mode);
+    SQLexer(SQSharedState *ss, SQCompilationContext &ctx);
     ~SQLexer();
     void Init(SQSharedState *ss, const char *code, size_t codeSize);
     SQInteger Lex();
@@ -74,16 +35,12 @@ private:
     SQInteger ReadID();
     SQInteger ReadDirective();
     void Next();
-    void AppendPosDirective(sqvector<SQChar> & vec);
-    bool ProcessReaderMacro();
-    void ExitReaderMacro();
     static SQInteger readf(void *);
     SQInteger AddUTF8(SQUnsignedInteger ch);
     SQInteger ProcessStringHexEscape(SQChar *dest, SQInteger maxdigits);
     SQInteger _curtoken;
     SQTable *_keywords;
     SQBool _reached_eof;
-    SQLexerMacroState macroState;
     SQCompilationContext &_ctx;
     const char *_sourceText;
     size_t _sourceTextSize;
@@ -100,7 +57,6 @@ public:
     unsigned _prevflags;
     unsigned _flags;
     enum SQLexerState _state;
-    enum SQLexerMode _mode;
     const SQChar *_svalue;
     SQInteger _nvalue;
     SQFloat _fvalue;
